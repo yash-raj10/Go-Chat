76b2090e0ccb7d22cf594ce6d88f7d8c598762a0
yash <yashraj.se10@gmail.com>
2024-04-30 01:28:03 +0530

    redis initialisation & parsing

--- pkg/redisrepo/key.go
+++ pkg/redisrepo/key.go
@@ -1,10 +1,27 @@
-package key
+package redisrepo
 
 import (
 	"fmt"
 	"time"
 )
 
+func userSetKey() string{
+	return "users"
+}
+
+func sessionKey(client string) string{
+	return "session#" + client
+}
+
 func chatKey() string{
 	return fmt.Sprintf("chat#%d", time.Now().UnixMilli())
 }
+
+func chatIndex() string{
+	return "id#chats"
+} 
+
+func contactListZKey(username string) string {
+	return "contact:" + username
+}
+
